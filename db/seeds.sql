c6a5ea3a83788141e18d7de9a4b42a21e9bb942b
Meredith Erb <mereditherb@merediths-mbp.attlocal.net>
2017-03-15 20:21:40 -0400

    kind of working burger app, items move if they've been eaten but can't add any new ones yet

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beefy McBeeferson', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Beefer', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Wrapped Burger', true);
