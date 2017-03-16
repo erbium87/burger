a1131fd4ede54f1835896c837acc95b23fbb7fbd
Meredith Erb <mereditherb@guest-2-00253.dhcp.unc.edu>
2017-03-16 17:08:28 -0400

    updated name in index.handlebars to correct column name to update db. can now add burger to db and page

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE burgers
 (
 `id` INT AUTO_INCREMENT NOT NULL,
 `burger_name` varchar(50) NOT NULL,
-`devoured` BOOLEAN NOT NULL,
+`devoured` BOOLEAN DEFAULT FALSE,
 `date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 PRIMARY KEY (`id`)
 );
